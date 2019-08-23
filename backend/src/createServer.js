f374219c42d6a9c5ccb904418f58cc454211fc19
jovenne <jovenneregs@gmail.com>
2019-08-23 16:16:07 +0800

    Remove comments in createServer.js

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -3,12 +3,9 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const db = require('./db');
 
-// Create the GraphQL Yoga Server
 function createServer() {  
-  //console.log("CREATE_SERVER.js", { Query } );
-
+  
   return new GraphQLServer({
-    // typeDefs: 'src/schema.graphql',
     typeDefs: 'src/generated/prisma.graphql',
     resolvers: {
       Query,
@@ -17,9 +14,6 @@ function createServer() {
     resolverValidationOptions: {
       requireResolversForResolveType: false,
     },
-    // ctx args in Query-js.txt is linking from this file
-    // so, ctx.db.query.user is connecting to my Graphql server
-    // ctx === context
     context: req => ({ 
       ...req, 
       db 
