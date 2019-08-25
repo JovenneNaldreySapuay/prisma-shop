5a5257dfe54ca6b63a098ad08373ffd1005289c4
jovenne <jovenneregs@gmail.com>
2019-08-25 19:55:52 +0800

    Add browser cookies for authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,57 +1,45 @@
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
 const createServer = require('./createServer');
 const db = require('./db');
+require('dotenv').config();
 
 const server = createServer();
+
 server.express.use(cookieParser());
 
-// decode the JWT so we can get the user Id on each request
+// get cookies
 server.express.use((req, res, next) => {
-  console.log("REQUEST in index.js", req );
   const { token } = req.cookies;
-  console.log("TOKEN in index.js:", token );
-
+ 
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    // put the userId onto the req for future requests to access
     req.userId = userId;
   }
   next();
 });
 
-// console.log("APP_SECRET= ", process.env.APP_SECRET );
-// console.log("FRONTEND_URL= ", process.env.FRONTEND_URL );
-
-// 2. Create a middleware that populates the user on each request
-
-// READ Docs Here about server.express.use() MIDDLEWARE !
-// npmjs.com/package/graphql-yoga#how-to-eject-from-the-standard-express-setup
-
 server.express.use(async (req, res, next) => {
-  // if they aren't logged in, skip this
   if (!req.userId) return next();
   const user = await db.query.user(
     { where: { id: req.userId } },
     '{ id, email, name }'
   );
   req.user = user;
-  console.log("SERVER.EXPRESS.USE", user );
+  
   next();
 });
 
 server.start(
   {
     cors: {
+      port: 4000,
       credentials: true,
-      origin: process.env.FRONTEND_URL,
+      origin: process.env.FRONTEND_URL
     },
   },
-  deets => {
-    // console.log("Deets:", deets );
-    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  serve => {
+    console.log(`Server is running on http://localhost:${serve.port}`);
   }
 );
 
