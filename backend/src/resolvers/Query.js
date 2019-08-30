c8b7ced8880a7dcfe3da53299ccc92452793e0d8
jovenne <jovenneregs@gmail.com>
2019-08-30 15:17:25 +0800

    Update Query resolver that can fetch single/multiple users/items

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,24 +1,34 @@
-//const { forwardTo } = require('prisma-binding');
-//const { hasPermission } = require('../utils');
-
-// Resolver tuts: 
-// https://www.prisma.io/tutorials/a-guide-to-common-resolver-patterns-ct08/
+const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
+  items: forwardTo('db'),
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
+
+  async me(parent, args, ctx, info) {
+
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in.');
+    }
+
+    return ctx.db.query.user(
+      { where: { id: ctx.request.userId } }, info
+    );
+  },  
   async users(parent, args, ctx, info) {
     // 1. Check if they are logged in
-    // if (!ctx.request.userId) {
-    //   throw new Error('You must be logged in, dude!');
-    // }
-    // console.log(ctx.request.userId);
-    // // 2. Check if the user has the permissions to query all the users
-    // hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in.');
+    }
+    console.log("users()", ctx.request.userId);
 
-    // 2. if they do, query all the users!
-    console.log("CONTEXT PARAMS: ", ctx.db.query );
+    // 2. Check if the user has the permissions
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
 
+    // 3. Query all the users
     return ctx.db.query.users({}, info);
-  },
+  }
 };
 
 module.exports = Query;
